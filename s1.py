715dbbbd23257eef0e67cb67e60b110365dc4d21
Call刘sir了 <citccld@163.com>
2021-02-21 22:55:00 +0800

    add s1

--- s1.py
+++ s1.py
@@ -91,37 +91,67 @@ for k,v_list in data.items():
 print()
 from pandas import DataFrame
 import numpy as np
-df = DataFrame(b_list)
-df2 = DataFrame(df.values.T,index=['service_name','current_package_size','target_package_size'])
-# df2.apply(lambda x:summary(x),axis=1)
-df2['GDC'] = df2[0]+df2[4]
-# 提取第0行
-print(df2)
-print('++++++++++++++++')
-df3 = df2.iloc[2]
-print(df3.values)
-print('================')
-df4 = df2.iloc[:,0]
-df5 = df2.iloc[:,4]
-x = df4.values.tolist()
-y = df5.values.tolist()
-x.extend(y)
-print(x)
+# df = DataFrame(b_list)
+# df2 = DataFrame(df.values.T,index=['service_name','current_package_size','target_package_size'])
+# # df2.apply(lambda x:summary(x),axis=1)
+# df2['GDC'] = df2[0]+df2[4]
+# # 提取第0行
+# print(df2)
+# print('++++++++++++++++')
+# df3 = df2.iloc[2]
+# print(df3.values)
+# print('================')
+# df4 = df2.iloc[:,0]
+# df5 = df2.iloc[:,4]
+# x = df4.values.tolist()
+# y = df5.values.tolist()
+# x.extend(y)
+# print(x)
 
 # print(df3.iloc[0:4])
 
 
 
-
-
-
-
-
-
-
-
-# b=list(b.values())
-# print(b)
-xuyao_dict = [
-    {'service_name': 'ADC','current_total_size':20,'target_total_size': 21,'ows_current_package_size':20,'ows_target_package_size': 21,'nawa_current_package_size':20,'nawa_target_package_size': 21}
+s1 = [
+    ['os',1,2,3,4,5,'Y'],
+    ['GDE服务',1,2,3,4,5,'Y'],
+    ['运维使能',1,2,3,4,5,'Y'],
+    ['镜像服务',1,2,3,4,5,'Y'],
 ]
+
+import numpy
+df = DataFrame(s1)
+# df0 = df[0] # 取0列
+df1 = df.iloc[0]  # 取0行
+df3 = df.iloc[3]
+df4 = df1 + df3
+
+def change_status(index_str):
+    if isinstance(index_str, numpy.int64):
+        return index_str
+    if 'N' in index_str:
+        index_str = 'N'
+    elif 'YY' in index_str:
+        index_str = 'Y'
+    return index_str
+
+
+df4 = df4.apply(lambda x:change_status(x))
+df4_reverse = df4.values.T
+
+# 删除os/镜像服务行，然后处理后合并
+df = df[(df.index!=0)&(df.index!=3)]  # 删除os/镜像服务行
+# 插入一行，随便设置索引，比如按照字符串作为索引
+# df.loc['new'] = ['duck', 10]
+# 此时需要把最后一行安排到第二行去，方法是新增一列指明他们的顺序
+# df['order'] = [1, 3, 4, 2]  # 所以这个顺序列表很重要
+# 按照order字段进行排序
+# df = df.sort_values(by='order')
+# 再接下来是重新设置索引
+# df = df.reset_index()
+# 最后删除多余的列
+# df.drop(columns=['index','order'],inplace=True)
+
+df.loc['new'] = df4_reverse
+print(df)
+
